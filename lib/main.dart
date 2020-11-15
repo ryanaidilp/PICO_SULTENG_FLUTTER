da56d7c1ab87c640621df5c2c8593d61ac2e5ce7
ryanaidilp <fajrianaidilp@gmail.com>
2020-11-15 18:31:07 +0800

    chore (code) :  implement onesignal and init localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
+import 'package:covid_app/views/views.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
-  Intl.defaultLocale = 'id_ID';
   await initializeDateFormatting();
+  WidgetsFlutterBinding.ensureInitialized();
+  Intl.defaultLocale = 'id_ID';
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  OneSignal.shared.init('ONE_SIGNAL_APP_ID',iOSSettings: {
+    OSiOSSettings.autoPrompt: false,
+    OSiOSSettings.inAppLaunchUrl: false
+  });
+  OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+
   runApp(MyApp());
 }
 
@@ -12,6 +22,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      title: 'PICO SulTeng',
+      debugShowCheckedModeBanner: false,
+      home: BottomNavScreen(),
+    );
   }
 }
