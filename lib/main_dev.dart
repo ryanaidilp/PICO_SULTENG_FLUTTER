7c495c251b61b145f0c5a95637b7f8b5a201adc4
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-30 12:28:31 +0800

    feat(main): create different main flavors

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,4 +1,5 @@
 import 'package:core/core.dart';
+
 import 'package:i10n/i10n.dart';
 import 'package:pico_sulteng_flutter/app.dart';
 import 'package:pico_sulteng_flutter/bootstrap.dart';
--- lib/main_stag.dart
+++ lib/main_stag.dart
@@ -1,4 +1,5 @@
 import 'package:core/core.dart';
+
 import 'package:i10n/i10n.dart';
 import 'package:pico_sulteng_flutter/app.dart';
 import 'package:pico_sulteng_flutter/bootstrap.dart';
@@ -6,6 +7,7 @@ import 'package:pico_sulteng_flutter/bootstrap.dart';
 void main() {
   bootstrap(
     () async {
+
       await Future.wait(
         [
           configureDependencies(),
