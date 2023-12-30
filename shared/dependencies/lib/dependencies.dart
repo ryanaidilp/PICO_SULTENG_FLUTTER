22e73281e8535ad0e1a7df104aa188b13d44b77a
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-30 12:06:18 +0800

    feat(shared): initialize dependencies related package

---
+++ shared/dependencies/lib/dependencies.dart
@@ -0,0 +1,20 @@
+/// {@template dependencies}
+/// Dependencies package for PICO SulTeng.
+/// {@endtemplate}
+library dependencies;
+
+export 'package:animate_icons/animate_icons.dart';
+export 'package:auto_route/auto_route.dart';
+export 'package:bloc/bloc.dart';
+export 'package:dartz/dartz.dart' show Either, Left, Right;
+export 'package:equatable/equatable.dart';
+export 'package:eva_icons_flutter/eva_icons_flutter.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:freezed_annotation/freezed_annotation.dart';
+export 'package:get_it/get_it.dart';
+export 'package:iconsax/iconsax.dart';
+export 'package:injectable/injectable.dart';
+export 'package:intl/intl.dart';
+export 'package:json_annotation/json_annotation.dart';
+export 'package:line_icons/line_icon.dart';
+export 'package:url_launcher/url_launcher.dart';
