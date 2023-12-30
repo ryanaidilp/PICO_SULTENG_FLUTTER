6ac12f9ffc476c01fa3723aa9e072728ae6d6536
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-30 12:05:44 +0800

    refactor(code): remove old codebase

--- lib/app/core/theme/color_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-final kVaccineColorPublic = Colors.pink.shade400;
-const kVaccineColorElderly = Colors.blueAccent;
-final kVaccineColorTeenager = Colors.pinkAccent.shade100;
-final kVaccineColorAll = Colors.green.shade700;
-const kVaccineColorPublicWorker = Colors.purple;
-final kVaccineColorHealthWorker = Colors.orange.shade700;
