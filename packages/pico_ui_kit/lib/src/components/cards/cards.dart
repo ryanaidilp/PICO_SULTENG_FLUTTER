8023c657a0548f3a886339075111b1245c144112
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-30 20:50:44 +0800

    feat(components): export test case card component

--- packages/pico_ui_kit/lib/src/components/cards/cards.dart
+++ packages/pico_ui_kit/lib/src/components/cards/cards.dart
@@ -1,3 +1,4 @@
 export 'case/pico_case_card.dart';
 export 'suspect/pico_suspect_card.dart';
+export 'test_case/pico_test_case_card.dart';
 export 'total_confirmed_case/pico_total_confirmed_case.dart';
