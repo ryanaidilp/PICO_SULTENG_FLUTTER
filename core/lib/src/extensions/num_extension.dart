9c3a2a739c7614446d96d5b95072795db90e3a33
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-31 10:58:34 +0800

    feat(extensions): add days & hours to int extension

--- core/lib/src/extensions/num_extension.dart
+++ core/lib/src/extensions/num_extension.dart
@@ -1,4 +1,6 @@
 extension IntX on int {
+  Duration get days => Duration(days: this);
+  Duration get hours => Duration(hours: this);
   Duration get minutes => Duration(minutes: this);
   Duration get seconds => Duration(seconds: this);
   Duration get milliseconds => Duration(milliseconds: this);
