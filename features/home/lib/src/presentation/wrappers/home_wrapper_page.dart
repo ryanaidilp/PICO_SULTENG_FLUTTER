f0848985b591cc4a921c17888ee05e6e1a4f3960
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-31 11:12:36 +0800

    refactor(wrappers): remove bloc provider from home wrapper page

--- features/home/lib/src/presentation/wrappers/home_wrapper_page.dart
+++ features/home/lib/src/presentation/wrappers/home_wrapper_page.dart
@@ -1,23 +1,12 @@
 import 'package:dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
-import 'package:home/src/presentation/blocs/banner/banner_bloc.dart';
 
 @RoutePage()
 class HomeWrapperPage extends StatelessWidget implements AutoRouteWrapper {
   const HomeWrapperPage({super.key});
 
   @override
-  Widget build(BuildContext context) => MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => BannerBloc()
-              ..add(
-                BannerEvent.load(),
-              ),
-          ),
-        ],
-        child: const AutoRouter(),
-      );
+  Widget build(BuildContext context) => const AutoRouter();
 
   @override
   Widget wrappedRoute(BuildContext context) => this;
