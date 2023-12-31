0f4b4f1599d8f3b83689e1ad4548679903a0b1f7
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-31 11:06:57 +0800

    feat(repositories-domain): create repository related to statistic

---
+++ features/statistic/lib/src/domain/repositories/statistic_repository.dart
@@ -0,0 +1,7 @@
+import 'package:core/core.dart';
+import 'package:dependencies/dependencies.dart';
+import 'package:statistic/src/domain/entities/statistic.dart';
+
+abstract class StatisticRepository {
+  Future<Either<Failure, Statistic>> latest();
+}
