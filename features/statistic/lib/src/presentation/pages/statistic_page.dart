00ac8a0f2be2b6c2d8468c89c5c07da93aad43bc
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-01-01 22:13:06 +0800

    refactor(page): make statistic page stateless

--- features/statistic/lib/src/presentation/pages/statistic_page.dart
+++ features/statistic/lib/src/presentation/pages/statistic_page.dart
@@ -3,85 +3,92 @@ import 'package:dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:i10n/i10n.dart';
 import 'package:pico_ui_kit/pico_ui_kit.dart';
-import 'package:statistic/src/presentation/blocs/blocs.dart';
-import 'package:statistic/src/presentation/widgets/statistic_detail_section.dart';
+import 'package:statistic/src/presentation/widgets/bar_chart_odp_and_pdp.dart';
+import 'package:statistic/statistic.dart';
 
 @RoutePage()
-class StatisticPage extends StatefulWidget {
+class StatisticPage extends StatelessWidget {
   const StatisticPage({
     super.key,
   });
 
   @override
-  State<StatisticPage> createState() => _StatisticPageState();
-}
-
-class _StatisticPageState extends State<StatisticPage> {
-  late final ScrollController _scrollController;
-  late final ValueNotifier<double> _titleVisibilityNotifier;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController = ScrollController()..addListener(_checkScrollOffset);
-    _titleVisibilityNotifier = ValueNotifier(0);
-  }
-
-  void _checkScrollOffset() {
-    if (_scrollController.hasClients) {
-      _titleVisibilityNotifier.value =
-          _scrollController.offset >= 20 ? 1.0 : 0.0;
-    }
-  }
-
-  @override
-  void dispose() {
-    _scrollController.removeListener(_checkScrollOffset);
-    _titleVisibilityNotifier.dispose();
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: ValueListenableBuilder(
-          valueListenable: _titleVisibilityNotifier,
-          builder: (_, value, child) => AnimatedOpacity(
-            opacity: value,
-            duration: 300.milliseconds,
-            child: child,
-          ),
-          child: BlocBuilder<LatestStatisticBloc, LatestStatisticState>(
-            builder: (context, state) => AnimatedSwitcher(
-              duration: 300.milliseconds,
-              child: switch (state) {
-                LatestStatisticLoadedState(data: final data) =>
-                  PicoUpdatedAtPlaceholder(
-                    date: DateHelper.buildUpdatedAtText(
-                      data.updatedAt.toLocal(),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            expandedHeight: 128.h,
+            scrolledUnderElevation: 10.h,
+            surfaceTintColor: context.picoColors.background.main,
+            shadowColor: context.picoColors.text.neutral.main.withOpacity(0.1),
+            title: BlocBuilder<LatestStatisticBloc, LatestStatisticState>(
+              builder: (context, state) => AnimatedSwitcher(
+                duration: 300.milliseconds,
+                child: switch (state) {
+                  LatestStatisticLoadedState(data: final data) =>
+                    PicoUpdatedAtPlaceholder(
+                      date: DateHelper.buildUpdatedAtText(
+                        data.updatedAt.toLocal(),
+                      ),
+                      label: context.i10n.data_case_label,
                     ),
-                    label: context.i10n.data_case_label,
-                  ),
-                LatestStatisticFailedState() => PicoUpdatedAtPlaceholder(
-                    date: '-',
-                    label: context.i10n.data_case_label,
-                  ),
-                _ => PicoUpdatedAtPlaceholder(
-                    date: '12 December 2023 12.05 WITA',
-                    label: context.i10n.data_case_label,
-                  ).sekeletonize(),
-              },
+                  LatestStatisticFailedState() => PicoUpdatedAtPlaceholder(
+                      date: '-',
+                      label: context.i10n.data_case_label,
+                    ),
+                  _ => PicoUpdatedAtPlaceholder(
+                      date: '12 December 2023 12.05 WITA',
+                      label: context.i10n.data_case_label,
+                    ).sekeletonize(),
+                },
+              ),
             ),
           ),
-        ),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16.r),
-        children: [
-          const StatisticDetailSection(),
-          24.verticalSpace,
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.r),
+                  child: const StatisticDetailSection(),
+                ),
+                Divider(
+                  color: context.picoColors.background.subtle,
+                  thickness: 8.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16.w,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            context.i10n.chart.title,
+                            style: PicoTextStyle.labelLg(),
+                          ),
+                          8.horizontalSpace,
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              context.i10n.buttons.more,
+                            ),
+                          ),
+                        ],
+                      ),
+                      8.verticalSpace,
+                      const BarChartODPAndPDP(),
+                      24.verticalSpace,
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
