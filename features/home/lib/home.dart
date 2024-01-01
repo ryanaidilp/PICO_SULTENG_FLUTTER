b2822adb48ad0f5fcb476132a8d96a7351cc80bb
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-01-01 16:55:33 +0800

    refactor(page): use reuseable widget

--- features/home/lib/home.dart
+++ features/home/lib/home.dart
@@ -4,6 +4,7 @@
 library home;
 
 export 'src/di/injector.module.dart';
+export 'src/domain/entities/entities.dart';
 export 'src/presentation/blocs/blocs.dart';
 export 'src/router/home_router.dart';
 export 'src/router/home_router.gm.dart';
