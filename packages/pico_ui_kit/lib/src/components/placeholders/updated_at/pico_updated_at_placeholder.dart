df3ac53374203883bd8fc7bcbb805c74c76177ba
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-01-01 16:55:50 +0800

    refactor(component): update updated at placeholder component

--- packages/pico_ui_kit/lib/src/components/placeholders/updated_at/pico_updated_at_placeholder.dart
+++ packages/pico_ui_kit/lib/src/components/placeholders/updated_at/pico_updated_at_placeholder.dart
@@ -5,29 +5,44 @@ import 'package:skeletonizer/skeletonizer.dart';
 
 class PicoUpdatedAtPlaceholder extends StatelessWidget {
   const PicoUpdatedAtPlaceholder({
+    required this.label,
     required this.date,
     super.key,
   });
 
+  final String label;
   final String date;
 
   @override
-  Widget build(BuildContext context) => Row(
+  Widget build(BuildContext context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Skeleton.keep(
             child: Text(
-              context.i10n.updated_at,
-              style: PicoTextStyle.bodySm(
-                color: context.picoColors.text.neutral.subtle,
-              ),
+              label,
+              style: PicoTextStyle.headingXs(),
             ),
           ),
-          4.horizontalSpace,
-          Text(
-            date,
-            style: PicoTextStyle.bodySm(
-              fontWeight: FontWeight.w400,
-            ),
+          24.verticalSpace,
+          Row(
+            children: [
+              Skeleton.keep(
+                child: Text(
+                  context.i10n.updated_at,
+                  style: PicoTextStyle.bodySm(
+                    color: context.picoColors.text.neutral.subtle,
+                  ),
+                ),
+              ),
+              4.horizontalSpace,
+              Text(
+                date,
+                style: PicoTextStyle.bodySm(
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
           ),
         ],
       );
