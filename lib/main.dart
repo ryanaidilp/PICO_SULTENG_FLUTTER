be95390a48dde571434d08e1e4406a33ef4aa418
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-01-02 03:06:49 +0800

    refactor(main): handling flavor init & native splash removal

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:config/config.dart';
 import 'package:core/core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:i10n/i10n.dart';
 import 'package:pico_sulteng_flutter/app.dart';
 import 'package:pico_sulteng_flutter/bootstrap.dart';
@@ -6,11 +8,13 @@ import 'package:pico_sulteng_flutter/bootstrap.dart';
 void main() {
   bootstrap(
     () async {
+      Flavor.flavor = FlavorStatus.PRODUCTION;
       await Future.wait(
         [
           configureDependencies(),
         ],
       );
+      FlutterNativeSplash.remove();
       return TranslationProvider(
         child: const App(),
       );
--- lib/main_stag.dart
+++ lib/main_stag.dart
@@ -1,4 +1,6 @@
+import 'package:config/config.dart';
 import 'package:core/core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:i10n/i10n.dart';
 import 'package:pico_sulteng_flutter/app.dart';
@@ -7,11 +9,13 @@ import 'package:pico_sulteng_flutter/bootstrap.dart';
 void main() {
   bootstrap(
     () async {
+      Flavor.flavor = FlavorStatus.STAGING;
       await Future.wait(
         [
           configureDependencies(),
         ],
       );
+      FlutterNativeSplash.remove();
       return TranslationProvider(
         child: const App(),
       );
