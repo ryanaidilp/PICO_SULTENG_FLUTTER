b3a5dd21a46b65252e7c9abe62a07d132549366c
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-01-02 03:03:38 +0800

    refactor(config): add flavor config

--- shared/config/lib/src/env/env.dart
+++ shared/config/lib/src/env/env.dart
@@ -4,18 +4,63 @@ part 'env.g.dart';
 
 @Envied(
   path: '.env',
-
-  /// Replace this to different environment
   obfuscate: true,
   useConstantCase: true,
+  name: 'EnvProd',
 )
-abstract class Env {
+abstract class EnvProd {
   @EnviedField()
-  static final String baseUrl = _Env.baseUrl;
+  static final String baseUrl = _EnvProd.baseUrl;
 
   @EnviedField()
-  static final String onesignalAppId = _Env.onesignalAppId;
+  static final String onesignalAppId = _EnvProd.onesignalAppId;
 
   @EnviedField()
-  static final String newsapiApiKey = _Env.newsapiApiKey;
+  static final String newsapiApiKey = _EnvProd.newsapiApiKey;
+
+   
+  @EnviedField()
+  static final String appName = _EnvProd.appName;
+}
+
+@Envied(
+  path: '.env.dev',
+  obfuscate: true,
+  useConstantCase: true,
+  name: 'EnvDev',
+)
+abstract class EnvDev {
+  @EnviedField()
+  static final String baseUrl = _EnvDev.baseUrl;
+
+  @EnviedField()
+  static final String onesignalAppId = _EnvDev.onesignalAppId;
+
+  @EnviedField()
+  static final String newsapiApiKey = _EnvDev.newsapiApiKey;
+  
+  @EnviedField()
+  static final String appName = _EnvDev.appName;
+}
+
+
+@Envied(
+  path: '.env.stg',
+  obfuscate: true,
+  useConstantCase: true,
+  name: 'EnvStg',
+)
+abstract class EnvStg {
+  @EnviedField()
+  static final String baseUrl = _EnvStg.baseUrl;
+
+  @EnviedField()
+  static final String onesignalAppId = _EnvStg.onesignalAppId;
+
+  @EnviedField()
+  static final String newsapiApiKey = _EnvStg.newsapiApiKey;
+
+   
+  @EnviedField()
+  static final String appName = _EnvStg.appName;
 }
---
+++ shared/config/lib/src/flavors/flavor.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+enum FlavorStatus {
+  DEVELOPMENT,
+  STAGING,
+  PRODUCTION;
+}
+
+class Flavor {
+  static FlavorStatus? flavor;
+}
