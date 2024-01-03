ed4b9c96a9b0bb08b5d5d32c0a7d9226e5e045ec
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-01-03 08:48:33 +0800

    feat(blocs): export latest national statistics bloc

--- features/statistic/lib/src/presentation/blocs/blocs.dart
+++ features/statistic/lib/src/presentation/blocs/blocs.dart
@@ -1,2 +1,3 @@
 export 'latest_covid_test/latest_covid_test_bloc.dart';
+export 'latest_national_statistic/latest_national_statistic_bloc.dart';
 export 'latest_statistic/latest_statistic_bloc.dart';
