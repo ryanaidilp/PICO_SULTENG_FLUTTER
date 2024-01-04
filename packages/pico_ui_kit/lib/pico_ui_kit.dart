4dae3db7e302b269ed24e2f600fe7489b128ef3c
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-01-04 21:46:46 +0800

    chore(packages): replace pull_to_refresh
    replace pull_to_refresh with pull_to_refresh_flutter3

--- packages/pico_ui_kit/lib/pico_ui_kit.dart
+++ packages/pico_ui_kit/lib/pico_ui_kit.dart
@@ -8,7 +8,7 @@ export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:flutter_spinkit/flutter_spinkit.dart';
 export 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 export 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-export 'package:pull_to_refresh/pull_to_refresh.dart';
+export 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 
 export 'src/components/components.dart';
 export 'src/style/styles.dart' show PicoTextStyle;
