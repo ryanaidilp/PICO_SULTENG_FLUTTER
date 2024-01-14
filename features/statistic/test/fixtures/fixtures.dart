98e18cccc32b9afd07eece093aaed2318d67eed7
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-01-14 11:59:30 +0800

    test(fixtures): crete fixture for covid test and statistic

---
+++ features/statistic/test/fixtures/fixtures.dart
@@ -0,0 +1,7 @@
+class StatisticFixture {
+  const StatisticFixture._();
+  static const String covidTest = 'covid_test_fixture.json';
+  static const String latestStatistic = 'latest_statistic_fixture.json';
+  static const String latestNationalStatistic =
+      'latest_national_statistic_fixture.json';
+}
