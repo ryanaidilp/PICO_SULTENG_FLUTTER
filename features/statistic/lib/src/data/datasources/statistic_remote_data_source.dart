8ba2988cdb41c3ffbe46b608d9f5a6efdfa87086
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-02-11 20:29:04 +0800

    refactor(datasources): add method to fetch all and allNational

--- features/statistic/lib/src/data/datasources/statistic_remote_data_source.dart
+++ features/statistic/lib/src/data/datasources/statistic_remote_data_source.dart
@@ -5,6 +5,8 @@ import 'package:statistic/src/data/models/statistic_model.dart';
 abstract class StatisticRemoteDataSource {
   Future<ApiResponseModel<StatisticModel>> latest();
   Future<ApiResponseModel<StatisticModel>> latestNational();
+  Future<ApiResponseModel<List<StatisticModel>>> all();
+  Future<ApiResponseModel<List<StatisticModel>>> allNational();
 }
 
 @LazySingleton(as: StatisticRemoteDataSource)
@@ -30,6 +32,46 @@ class StatisticRemoteDataSourceImpl implements StatisticRemoteDataSource {
     final result = ApiResponseModel<StatisticModel>.fromJson(
       response,
       (json) => StatisticModel.fromJson(json! as JSON),
+    );
+
+    return result;
+  }
+
+  @override
+  Future<ApiResponseModel<List<StatisticModel>>> all() async {
+    final response = await _httpModule.get(ApiEndpoint.historyProvince());
+
+    final result = ApiResponseModel<List<StatisticModel>>.fromJson(
+      response,
+      (json) {
+        if (json is! List) {
+          return [];
+        }
+
+        final data = json.map((e) => StatisticModel.fromJson(e as JSON));
+
+        return data.toList();
+      },
+    );
+
+    return result;
+  }
+
+  @override
+  Future<ApiResponseModel<List<StatisticModel>>> allNational() async {
+    final response = await _httpModule.get(ApiEndpoint.history());
+
+    final result = ApiResponseModel<List<StatisticModel>>.fromJson(
+      response,
+      (json) {
+        if (json is! List) {
+          return [];
+        }
+
+        final data = json.map((e) => StatisticModel.fromJson(e as JSON));
+
+        return data.toList();
+      },
     );
 
     return result;
