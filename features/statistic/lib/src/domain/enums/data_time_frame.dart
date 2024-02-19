9f05db7af73d76bf3358105cc7b64443bd9c9741
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-02-19 13:03:06 +0800

    feat(enums): add case type and data time frame enums

---
+++ features/statistic/lib/src/domain/enums/data_time_frame.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:i10n/i10n.dart';
+
+enum DataTimeFrame {
+  oneMonth,
+  threeMonth,
+  sixMonth,
+  all;
+
+  String label(BuildContext context) => switch (this) {
+        oneMonth => context
+            .translations.chart.line_chart.filter_label.timeframe.one_month,
+        threeMonth => context
+            .translations.chart.line_chart.filter_label.timeframe.three_month,
+        sixMonth => context
+            .translations.chart.line_chart.filter_label.timeframe.six_month,
+        _ => context.translations.chart.line_chart.filter_label.all,
+      };
+
+  int get offset => switch (this) {
+        oneMonth => 30,
+        threeMonth => 90,
+        sixMonth => 180,
+        _ => 0,
+      };
+}
