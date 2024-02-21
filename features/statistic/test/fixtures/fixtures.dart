602400d83eea361b7c59cbeefb68fb5e03bd8f4a
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-02-22 02:24:48 +0800

    test(fixture): create fixture for all statistic

--- features/statistic/test/fixtures/fixtures.dart
+++ features/statistic/test/fixtures/fixtures.dart
@@ -1,8 +1,9 @@
 class StatisticFixture {
   const StatisticFixture._();
-  static const String failed = 'failed_fixture.json';
+  static const String allStatistic = 'all_statistic_fixture.json';
   static const String covidTest = 'covid_test_fixture.json';
-  static const String latestStatistic = 'latest_statistic_fixture.json';
+  static const String failed = 'failed_fixture.json';
   static const String latestNationalStatistic =
       'latest_national_statistic_fixture.json';
+  static const String latestStatistic = 'latest_statistic_fixture.json';
 }
