95f634cfc4d2ae67575bd093df68ac2ae92943b7
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-02-22 10:20:21 +0800

    feat(usecases): create get regencies by province code usecase

--- features/statistic/lib/src/domain/usecases/usecases.dart
+++ features/statistic/lib/src/domain/usecases/usecases.dart
@@ -3,3 +3,4 @@ export 'get_all_statistic.dart';
 export 'get_latest_covid_test.dart';
 export 'get_latest_national_statistic.dart';
 export 'get_latest_statistic.dart';
+export 'get_regencies_by_province.dart';
