4f8ba625b382fda96b0c12ad6e823a589f93880c
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-02-22 10:19:40 +0800

    feat(models): create regency model

--- features/statistic/lib/src/data/models/models.dart
+++ features/statistic/lib/src/data/models/models.dart
@@ -2,5 +2,6 @@ export 'case_model.dart';
 export 'covid_test_model.dart';
 export 'percentage_model.dart';
 export 'recap_model.dart';
+export 'regency_model.dart';
 export 'reproduction_number_model.dart';
 export 'statistic_model.dart';
