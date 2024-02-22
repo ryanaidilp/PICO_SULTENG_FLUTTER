b50f90a9f1b9eddf9bfb194ce2ea9e97e14263f5
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-02-22 10:20:41 +0800

    test(fixtures): create fixture for regency by province

--- features/statistic/test/fixtures/fixtures.dart
+++ features/statistic/test/fixtures/fixtures.dart
@@ -1,6 +1,8 @@
 class StatisticFixture {
   const StatisticFixture._();
   static const String allStatistic = 'all_statistic_fixture.json';
+  static const String regenciesByProvince =
+      'regencies_by_province_fixture.json';
   static const String covidTest = 'covid_test_fixture.json';
   static const String failed = 'failed_fixture.json';
   static const String latestNationalStatistic =
